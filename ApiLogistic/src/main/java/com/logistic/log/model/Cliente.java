7c0ccda55845e2a838695ba8ad6f448bcdaaa76e
Viviane Emilie <vivi29emi@gmail.com>
2022-07-19 22:49:46 -0300

    Adicionando pastas para criação do repositório estudo spring

---
+++ ApiLogistic/src/main/java/com/logistic/log/model/Cliente.java
@@ -0,0 +1,13 @@
+package com.logistic.log.model;
+
+
+import lombok.Data;
+
+@Data
+public class Cliente {
+	
+	
+	private String nome;
+	private String email;
+	private String telefone;
+}
---
+++ ApiLogistic/src/test/java/com/logistic/log/ApiLogisticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistic.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLogisticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
