eba75aa3f157e8be034a9f40b627cd812bba622b
Viviane Emilie <vivi29emi@gmail.com>
2022-07-24 16:48:39 -0300

    Configurado a camada cliente com o relacionamento com a tabela entrega

--- ApiLogistic/src/main/java/com/logistic/log/model/Cliente.java
+++ ApiLogistic/src/main/java/com/logistic/log/model/Cliente.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -34,7 +35,6 @@ public class Cliente {
 	
 	@Size(max= 20)
 	private String telefone;
-	
 	
 	
 }
