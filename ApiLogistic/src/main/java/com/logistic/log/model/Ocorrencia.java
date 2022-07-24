a2a15d512e0cb147d3add6aac59676fb6b02dd1b
Viviane Emilie <vivi29emi@gmail.com>
2022-07-24 17:03:18 -0300

    Entidade adicionada para registro de ocorrencia da entrega

---
+++ ApiLogistic/src/main/java/com/logistic/log/model/Ocorrencia.java
@@ -0,0 +1,30 @@
+package com.logistic.log.model;
+
+import java.time.OffsetDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Ocorrencia {
+	
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	private Entrega entrega;
+	
+	private String descricao;
+	private OffsetDateTime dataRegistro;
+
+}
