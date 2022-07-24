c86df7ac61206fa14d9f3d5f4b15b7d51ba166da
Viviane Emilie <vivi29emi@gmail.com>
2022-07-24 17:07:16 -0300

    Adicionada a entidade entrega

---
+++ ApiLogistic/src/main/java/com/logistic/log/repository/EntregaRepository.java
@@ -0,0 +1,11 @@
+package com.logistic.log.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.logistic.log.model.Entrega;
+
+@Repository
+public interface EntregaRepository extends JpaRepository < Entrega,Long> {
+
+}
