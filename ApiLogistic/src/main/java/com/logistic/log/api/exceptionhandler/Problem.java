dc1219397bb86c6d4c3730f7d0c4312d836cf9e2
Viviane Emilie <vivi29emi@gmail.com>
2022-07-24 16:47:37 -0300

    Camada problem adicionada para configurar as mensagens de erro do exceptionHandle

--- ApiLogistic/src/main/java/com/logistic/log/api/exceptionhandler/Problem.java
+++ ApiLogistic/src/main/java/com/logistic/log/api/exceptionhandler/Problem.java
@@ -1,6 +1,6 @@
 package com.logistic.log.api.exceptionhandler;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -8,14 +8,14 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
- // Irá retornar no body a respota do erro ou seja o campo que está errado
+ // Irá retornar no body a resposta do erro ou seja o campo que não foi colocado a requisiçao
 
 @JsonInclude(Include.NON_NULL)// <- quando os atributos estiverem null não vai aparecer.
 @Data
 public class Problem {
 	
 	private Integer status;
-	private LocalDateTime dataHora; //<- Hora que requisição foi feita
+	private OffsetDateTime dataHora; //<- Hora que requisição foi feita
 	private String titulo;
 	private List<Campo>Campos;
 	
