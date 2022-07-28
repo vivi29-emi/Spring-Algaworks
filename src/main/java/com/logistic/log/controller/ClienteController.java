eabbdbf7775c891033039ff62a2ff565ba5f29cc
Viviane Emilie <vivi29emi@gmail.com>
2022-07-28 11:52:21 -0300

    Configurado a camada controllerCliente o crud de excluir cliente

--- src/main/java/com/logistic/log/controller/ClienteController.java
+++ src/main/java/com/logistic/log/controller/ClienteController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -66,7 +67,7 @@ public class ClienteController {
 		return ResponseEntity.ok(cliente);
 		
 	}
-	
+	@DeleteMapping("/{clienteId}")
 	public ResponseEntity<Void> remover ( @PathVariable Long clienteId){
 		if(!clienteRepository.existsById(clienteId)) {
 			return ResponseEntity.notFound().build();
